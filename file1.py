33efac232a040af1b41b21881616e76f664e9f04
arnav sood <me.arnavsood@yahoo.com>
2021-03-05 00:23:00 +0530

    explecit

--- file1.py
+++ file1.py
@@ -1,7 +1,7 @@
 print("this is dev changed")
-<<<<<<< HEAD
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 print("writing to explicit change")
-=======
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>s
 print("done explicit commit")
 >>>>>>> 6aa4375f99c8a88ea2319089bcfd262be3f50a73
 print("this is file daklfjq;adss")
