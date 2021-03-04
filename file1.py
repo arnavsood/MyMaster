b0286376b84bf9a6e7b5ece6178539e1051e2c5c
arnav sood <43279540+arnavsood@users.noreply.github.com>
2021-03-05 00:27:05 +0530

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,14 +1,1 @@
 print("this is dev changed")
-<<<<<<< HEAD
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-print("writing to explicit change")
->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>s
-print("done explicit commit")
->>>>>>> 6aa4375f99c8a88ea2319089bcfd262be3f50a73
-=======
-print("cleaned")
-print("writing to explicit change")
-=======
-
->>>>>>> 52272610d8968ee24f31b49de42f4e10b1380d31
-print("this is file daklfjq;adss")
